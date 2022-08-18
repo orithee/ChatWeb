18b145b14f5727c855968b8a222a72f0d1ad9b3a
Ori Thee <orithee@cyber4s.dev>
2022-08-18 17:17:00 +0300

    Download and start code of 'WebSocket'

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,18 +1,33 @@
 import express, { Request, Response } from 'express';
 import { json } from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { join } from 'path';
+dotenv.config({ path: join(__dirname, '../../.env') });
 
-const server = express();
-server.use(json());
-server.use(express.static('./dist'));
-server.use(cors());
-
-// Get requests:
-server.get('/get', async (req: Request, res: Response) => {
-  console.log('get request');
-  res.send({ message: true });
-});
+const { Server } = require('ws');
 
 // Initialize the server + port:
-const port = process.env.PORT || 4000;
-server.listen(port, () => console.log('Hosted: http://localhost:' + port));
+const PORT = process.env.PORT;
+const server = express()
+  .use((req: Request, res: Response) => {
+    console.log('Request: ' + req.path);
+    res.sendFile(req.path || 'index.html', {
+      root: './dist',
+    });
+  })
+  .listen(PORT, () => console.log(`Hosted: http://localhost:${PORT}`));
+
+// Turning the express server to 'WebSocket':
+const ws = new Server({ server });
+
+ws.on('connection', (client: any) => {
+  console.log('Client connected');
+  client.send('Hey client from WebSocket!');
+
+  client.on('message', (msg: string) => {
+    console.log('Message from client: ' + msg);
+  });
+
+  client.on('close', () => console.log('Client disconnected'));
+});

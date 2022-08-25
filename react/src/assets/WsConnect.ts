80c9efab2184122c80b19473c99093b1012c2232
Ori Thee <orithee@cyber4s.dev>
2022-08-25 12:32:02 +0300

    Add a function that sends the 'token' cookie if exists

--- react/src/assets/WsConnect.ts
+++ react/src/assets/WsConnect.ts
@@ -2,21 +2,24 @@ export function WsConnect() {
   let url: string = '';
   if (process.env.NODE_ENV === 'development') {
     url = 'ws://localhost:4000';
-    console.log('development: ');
-    console.log(url);
+    console.log('development:', url);
   } else {
     url = window.location.origin.replace(/^http/, 'ws');
-    console.log('production:');
-    console.log(url);
+    console.log('production:', url);
   }
 
   let ws = new WebSocket(url);
-  console.log('WsConnect');
-
   ws.onopen = () => {
     console.log('WebSocket connected successfully');
-    ws.send('Hello from client!');
+    ws.send(JSON.stringify({ type: 'initial', token: getCookie('token') }));
   };
 
   return ws;
+}
+
+function getCookie(name: string): string | undefined {
+  // A function that gets the cookie name and returns its value if exists:
+  const re = new RegExp(name + '=([^;]+)');
+  const value = re.exec(document.cookie);
+  return value != null ? unescape(value[1]) : undefined;
 }

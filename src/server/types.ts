652840b0dfe9cec7916ed9ed78aff56fc6b28a43
Ori Thee <orithee@cyber4s.dev>
2022-08-25 17:11:19 +0300

    Change the file name and add 'client type'

--- src/server/types.ts
+++ src/server/types.ts
@@ -1,3 +1,10 @@
+// Client type:
+export type Client = {
+  send: (arg0: string) => void;
+  on: (arg0: string, arg1: { (msg: any): void }) => void;
+};
+
+// Messages Types:
 export type MessageTypes = Initial | Register | Login | Error;
 
 interface Login {

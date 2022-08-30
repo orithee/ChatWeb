5379d8e516bbcef5dd525a848b9930ff45dbe337
Ori Thee <orithee@cyber4s.dev>
2022-08-30 22:14:20 +0300

    Fixing a little bug

--- src/server/server.ts
+++ src/server/server.ts
@@ -86,6 +86,12 @@ async function registerFunction(client: Client, message: Register) {
     if (await createUser(message)) {
       client.send(
         toStr({
+          type: 'groupList',
+          list: [],
+        })
+      );
+      client.send(
+        toStr({
           type: 'login',
           username: message.username,
           token: sha1(message.password + message.username),

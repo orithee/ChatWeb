24bf814d2c9f3b7b72442b018eb047e738398918
Ori Thee <orithee@cyber4s.dev>
2022-09-02 09:05:15 +0300

    Adding input of sending a message

--- react/src/components/chat/Chat.tsx
+++ react/src/components/chat/Chat.tsx
@@ -1,15 +1,31 @@
 import style from './Chat.module.scss';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { chatState } from '../../redux/store';
 import { useContext, useState } from 'react';
 import { WsConnection } from '../../App';
 import ListGroup from 'react-bootstrap/esm/ListGroup';
+import { toStr } from '../../assets/auxiliaryFunc';
 
 function Chat() {
   const connection = useContext<WebSocket>(WsConnection);
   const messages = useSelector((state: chatState) => state.chat.groupMessages);
   let groupIdParam = useParams().groupId;
+  const [inputMsg, setInputMsg] = useState<string>('');
+
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!inputMsg) return;
+    console.log(inputMsg);
+    connection.send(
+      toStr({
+        type: 'chatMessage',
+        group: groupIdParam,
+        message: inputMsg,
+      })
+    );
+    setInputMsg('');
+  };
 
   return (
     <div className={style.container}>
@@ -34,7 +50,16 @@ function Chat() {
             );
           })}
       </div>
-      <div className={style.bottom}>bottom</div>
+      <div className={style.bottom}>
+        <form onSubmit={(e) => submitForm(e)}>
+          <input
+            type="text"
+            onChange={(e) => setInputMsg(e.target.value)}
+            value={inputMsg}
+            placeholder="Type a message"
+          />
+        </form>
+      </div>
 
       {/* TODO:
           1. Option to send message - insert to group_messages table.

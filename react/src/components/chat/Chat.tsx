9dfd898a119205b9d0ca4bfb00c51ea641b0504f
Ori Thee <orithee@cyber4s.dev>
2022-09-02 08:24:05 +0300

    Great improvement of the design and structure of the chat + todo's

--- react/src/components/chat/Chat.tsx
+++ react/src/components/chat/Chat.tsx
@@ -15,36 +15,35 @@ function Chat() {
     <div className={style.container}>
       <div className={style.up}>
         <div className={style.up_left}>
-          {/* <img src="" alt="" /> */}
+          {/* TODO: Add profile img option */}
           <p>{groupIdParam}</p>
         </div>
       </div>
       <div className={style.main}>
-        <div>main:</div>
-        {groupIdParam}
-        <br />
-        <div>
-          {messages &&
-            messages.length != 0 &&
-            messages.map((message, index) => {
-              return (
-                <ListGroup.Item key={index}>
-                  <div>{message.group_name}</div>
-                  <div>{message.sent_by}</div>
-                  <div>{message.message_id}</div>
-                  <div>{message.created_at}</div>
-                  <div>{message.message_text}</div>
-                </ListGroup.Item>
-              );
-            })}
-        </div>
-        <div className={style.message}>
-          <div className={style.sent_by}>sent by</div>
-          <div className={style.text}>message text</div>
-          <div className={style.hour}>time</div>
-        </div>
+        {messages &&
+          messages.length != 0 &&
+          messages.map((message, index) => {
+            return (
+              <ListGroup.Item className={style.message1} key={index}>
+                <div className={style.message}>
+                  <div className={style.sent_by}>{message.sent_by}</div>
+                  <div className={style.text}>{message.message_text}</div>
+                  <div className={style.hour}>{message.created_at}</div>
+                </div>
+              </ListGroup.Item>
+            );
+          })}
       </div>
       <div className={style.bottom}>bottom</div>
+
+      {/* TODO:
+          1. Option to send message - insert to group_messages table.
+          2. Implement it by send the message to all clients that connected.
+          3. UseEffect  - every message: insert into database, sent from server to all, 
+             update the group_messages if this group is open.
+          4. If this group does not opening now - add green indication about the message on the group list.
+          5.
+      */}
     </div>
   );
 }

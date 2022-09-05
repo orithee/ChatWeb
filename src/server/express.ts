45756351a51fb79efe3656704f3acccb8894c05d
Ori Thee <orithee@cyber4s.dev>
2022-09-05 11:45:20 +0300

    Fix bug on deploy to heroku

--- src/server/express.ts
+++ src/server/express.ts
@@ -10,9 +10,12 @@ export const expressServer = express()
   .use((req: Request, res: Response) => {
     console.log('Request in expressServer:', req.path);
     if (process.env.NODE_ENV === 'production') {
-      res.sendFile(req.path || 'index.html', {
-        root: './dist',
-      });
+      const route = (str: string) => req.path.toString().startsWith(str);
+      if (route('/main') || route('/register') || route('/login')) {
+        res.sendFile('/index.html', { root: './dist' });
+      } else {
+        res.sendFile(req.path || '/index.html', { root: './dist' });
+      }
     }
   })
   .listen(PORT, () => console.log(`Hosted: http://localhost:${PORT}`));
